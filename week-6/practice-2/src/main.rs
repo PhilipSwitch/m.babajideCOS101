15ab2675e8f4d365f0fe7c338e198f27603a037f
PhilipSwitch <mobolaji.babajide@pau.edu.ng>
2025-11-03 11:36:32 +0100

    (My sixth commit)

--- week-6/practice-2/src/main.rs
+++ week-6/practice-2/src/main.rs
@@ -1,3 +1,8 @@
 fn main() {
-    println!("Hello, world!");
+    let empty_string = String::new();
+    println!("Length of empty_string is {}",empty_string.len());
+
+    let content_string = String::from("COmputerScience");
+    println!("Length of content_stringis {}",content_string.len());
+    
 }
---
+++ week-6/practice-2/target/debug/deps/practice_2-d9f061611743d4f8.d
@@ -0,0 +1,5 @@
+C:\Users\NEW\Documents\m.babajideCOS101\week-6\practice-2\target\debug\deps\practice_2-d9f061611743d4f8.d: src\main.rs
+
+C:\Users\NEW\Documents\m.babajideCOS101\week-6\practice-2\target\debug\deps\libpractice_2-d9f061611743d4f8.rmeta: src\main.rs
+
+src\main.rs:
