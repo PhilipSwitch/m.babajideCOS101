5e08f9f43db86e432289d9b746fb39836cdd96e1
PhilipSwitch <mobolaji.babajide@pau.edu.ng>
2025-11-03 11:49:03 +0100

    (My sixth commit)

--- week-6/practice-4/src/main.rs
+++ week-6/practice-4/src/main.rs
@@ -1,3 +1,17 @@
 fn main() {
-    println!("Hello, world!");
+   
+   let fullname = "CHibudum John Umeh";
+   let depatment = "Computer Science";
+   let uni = "PAn-Atlantic University";
+
+   let mut school = "School of Science".to_string();
+   //push string
+   school.push_str(" and Technology");
+
+   println!("My name is: {}", fullname);
+   // check length
+   println!("The length my full name is: {}",fullname.len());
+   println!("I am a student of {} Department", depatment);
+   println!("{}",school);
+   println!("{}",uni);  
 }
